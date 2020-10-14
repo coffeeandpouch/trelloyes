a76b99b6884dd13c4c5cdb3e258d6baf352cc9ee
Sam Einsidler <sameinsidler@me.com>
2020-10-14 11:43:52 -0500

    Add files via upload

--- List.js
+++ List.js
@@ -1,8 +1,6 @@
 import React from "react";
-import card from "./Card";
 import Card from "./Card";
 import "./List.css";
-
 export default function List(props) {
   return (
     <section className="List">
@@ -10,13 +8,27 @@ export default function List(props) {
         <h2>{props.header}</h2>
       </header>
       <div className="List-cards">
-        {props.cards.map((Card) => (
-          <Card key={card.id} title={card.title} content={card.content} />
+        {props.cards.map((card) => (
+          <Card
+            key={card.id}
+            id={card.id}
+            title={card.title}
+            content={card.content}
+            onClickDelete={props.onClickDelete}
+          />
         ))}
-        <button type="button" className="List-add-button">
-          +Add Random Card
+        <button
+          type="button"
+          className="List-add-button"
+          onClick={() => props.onClickAdd(props.id)}
+        >
+          + Add Random Card
         </button>
       </div>
     </section>
   );
 }
+
+List.defaultProps = {
+  onClickAdd: () => {},
+};
